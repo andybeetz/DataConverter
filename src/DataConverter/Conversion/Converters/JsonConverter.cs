623eb2e2012fdc34cf9f14cfd1fd1dbaf6cf492a
Andy Beetz <andy@beetz.org.uk>
2020-06-22 22:44:08 +0100

    Implementing IInputconverter of JsonConverter

--- src/DataConverter/Conversion/Converters/JsonConverter.cs
+++ src/DataConverter/Conversion/Converters/JsonConverter.cs
@@ -1,7 +1,9 @@
 ﻿using DataConverter.Interfaces;
 using DataConverter.Model;
 
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
 
 using System.Collections.Generic;
 using System.IO;
@@ -9,7 +11,7 @@ using System.Linq;
 
 namespace DataConverter.Conversion.Converters
 {
-	public class JsonConverter : IOutputConverter
+	public class JsonConverter : IOutputConverter, IInputConverter
 	{
 		private IFileStreamProvider _fileStreamProvider;
 
@@ -64,6 +66,66 @@ namespace DataConverter.Conversion.Converters
 				writer.Write(jsonDoc.ToString());
 				writer.Flush();
 				writer.Close();
+			}
+
+			return true;
+		}
+
+		public bool GetInput(string inputLocation, out IEnumerable<DataRecord> inputData)
+		{
+			if(string.IsNullOrWhiteSpace(inputLocation))
+			{
+				inputData = null;
+				return false;
+			}
+
+			var inputStream = _fileStreamProvider.GetFileStream(inputLocation);
+			using(var reader = new StreamReader(inputStream))
+			{
+				var json = JArray.Parse(reader.ReadToEnd());
+
+				var jsonReader = json.CreateReader();
+
+				var parsedData = new List<DataRecord>();
+
+
+
+				foreach(var jsonRecord in json.Children())
+				{
+					var newRecord = new DataRecord();
+					var children = jsonRecord.Children();
+					foreach(var child in jsonRecord.Children())
+					{
+						if(child is JProperty)
+						{
+							var prop = child as JProperty;
+							if(prop.Value.Type == JTokenType.Object)
+							{
+								var itemGroup = new ItemGroup
+								{
+									Name = prop.Name
+								};
+
+								// get sub nodes
+								foreach(var subChild in prop.Value.Children())
+								{
+									var subProp = subChild as JProperty;
+									itemGroup.Items.Add(new SingleItem() { Name = subProp.Name, Value = (string)subProp.Value });
+								}
+								newRecord.Items.Add(itemGroup);
+							}
+							else if(prop.Value.Type == JTokenType.String)
+							{
+								// get single item
+								newRecord.Items.Add(new SingleItem() { Name = prop.Name, Value = (string)prop.Value });
+							}
+						}
+					}
+
+					parsedData.Add(newRecord);
+				}
+
+				inputData = parsedData;
 			}
 
 			return true;
